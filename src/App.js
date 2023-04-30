add10d3d7a237483ab279ce060753ed85d2a608f
Johannes Eriksson <johannes.einar.eriksson@gmail.com>
2023-04-30 12:45:39 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Message from "./components/Message";
+const roomsList = [
+  { name: "crypto23", color: "bg-green-500" },
+  { name: "politics18", color: "bg-red-500" },
+  { name: "weather99", color: "bg-yellow-500" },
+  { name: "soccer21", color: "bg-blue-500" },
+  { name: "tate5", color: "bg-orange-500" },
+  { name: "spider88", color: "bg-indigo-500" },
+];
 
 function App() {
+  const [mActive, setMactive] = useState(false);
+  const [room, setRoom] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="grid place-items-center h-[95vh] border">
+      {mActive ? (
+        <Message room={room} />
+      ) : (
+        <div className="flex flex-col">
+          <div className="flex flex-col items-center gap-6">
+            <h1 className="text-4xl font-semibold">
+              Secure chatrooms free for use
+            </h1>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                setMactive(true)
+              }}
+              className="flex items-center gap-1 justify-center"
+            >
+              <input
+                value={room}
+                onChange={(e) => setRoom(e.target.value)}
+                placeholder="#room"
+                className="w-[350px]"
+              />
+              <button type="submit" className="py-2 border hover:text-orange-600 font-semibold px-8">
+                join
+              </button>
+            </form>
+          </div>
+          <div className="grid grid-cols-3 place-items-center gap-2 mt-12">
+            {roomsList.map((name) => (
+              <div
+                key={name.name.length * Math.random()}
+                onClick={() => {
+                  setRoom(name.name);
+                  setMactive(true);
+                }}
+                className={`hover:scale-110 duration-100 cursor-pointer border p-2 rounded shadow-xl w-[200px] h-24 ${name.color} text-white font-semibold`}
+              >
+                {"#" + name.name}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </main>
   );
 }
 
---
+++ src/components/Message.js
@@ -0,0 +1,98 @@
+import { socket } from "../socket";
+import { useEffect, useState } from "react";
+
+function Message({ room }) {
+  const [input, setInput] = useState("");
+  const [userName, setUserName] = useState("");
+  const [con, setCon] = useState(false);
+  const [popup, setPopup] = useState(true);
+  const [messages, setMessages] = useState([{ msg: "", name: "", time: "" }]);
+
+  useEffect(() => {
+    socket.emit("room", room);
+    socket.on("connectRoom", (msg) => console.log(msg));
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit("send-msg", input);
+    setInput("");
+  };
+
+  const sign = () => {
+    if (userName === "") return;
+    socket.emit("new-user", userName);
+    setCon(true);
+    setPopup(false);
+  };
+
+  socket.on("chat", (msg) => {
+    appendMessage(msg.message, msg.name, msg.time);
+  });
+
+  const appendMessage = (msg, na, time) => {
+    if (msg === "") return;
+    const array = [{ msg: msg, name: na, time: time }, ...messages];
+    setMessages(array);
+  };
+
+  return (
+    <div className="m-8">
+      <div
+        className={`popup w-screen h-screen bg-black/70 absolute top-0 left-0 flex items-center gap-8 flex-col justify-center ${
+          popup ? "block" : "hidden"
+        }`}
+      >
+        <h1 className="text-white font-bold text-2xl">Welcome to Chat!</h1>
+        <div className="flex gap-3">
+          <input
+            placeholder="name"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <button onClick={sign}>connect</button>
+        </div>
+      </div>
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-3">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              sendMessage();
+            }}
+            className="flex gap-3"
+          >
+            <input
+              placeholder="message"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button type="submit">send</button>
+          </form>
+        </div>
+        <div>
+          <h3>logged in as: {userName}</h3>
+        </div>
+      </div>
+      {con && (
+        <div>
+          {messages[0].msg
+            ? messages.map((mes) => (
+                <p
+                  key={Math.random() * 20}
+                  className={`p-4 shadow-xl w-64 mt-4 border hover:border-orange-600 cursor-pointer rounded-md duration-75`}
+                >
+                  <div className="text-black/60 font-semibold text-sm flex justify-between">
+                    <p>{mes.name === userName ? "@you" : "@" + mes.name}</p>
+                    <p>{mes.time}</p>
+                  </div>
+                  <p className="text-lg">{mes.msg}</p>
+                </p>
+              ))
+            : "No messages to display..."}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Message;

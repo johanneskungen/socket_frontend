693705dc1698dd2054e97917ca79906b0a3f7162
Johannes Eriksson <johannes.einar.eriksson@gmail.com>
2023-05-01 19:23:54 +0200

    mm

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === "production"
-    ? "https://socket-backend-6kut.onrender.com"
+    ? "https://socket-backend-nw30.onrender.com"
     : `http://localhost:${process.env.PORT}`;
 
 export const socket = io(URL, {

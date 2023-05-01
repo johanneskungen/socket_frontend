f44c28e9de455dcd55ee080725fe724f0781e195
Johannes Eriksson <johannes.einar.eriksson@gmail.com>
2023-05-01 18:58:53 +0200

    testing

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,9 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:1000";
+  process.env.NODE_ENV === "production"
+    ? "https://socket-backend-6kut.onrender.com"
+    : `http://localhost:${process.env.PORT}`;
 
 export const socket = io(URL, {
   autoConnect: true,
